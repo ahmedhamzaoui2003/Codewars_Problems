03d6a2a909bc44231a0ea001a4fe2a57ba21f24b
ahmed hamzaoui <ahmedhamzaoui2003@gmail.com>
2026-05-25 18:34:01 +0100

    Add project files.

---
+++ Codewars_Problems/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Problems
+{
+    internal class Program
+    {
+
+        public static string Smash(string[] words)
+        {
+            string s = "";
+            foreach (string w in words)
+            {
+                s += w + " ";
+            }
+            s = s.Substring(0, s.Length - 1);
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] words = { "hello", "world", "this", "is", "great" };
+            Console.WriteLine(Smash(words));
+        }
+    }
+}
---
+++ Codewars_Problems/Solutions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Problems
+{
+    internal class Solutions
+    {
+        // Problem 1 : You only need one - Beginner
+        // 1st method : using some logic 
+        public static string Smash(string[] words)
+        {
+            string s = "";
+            foreach (string w in words)
+            {
+                s += w + " ";
+            }
+            s = s.Substring(0, s.Length - 1);
+            return s;
+        }
+        // 2nd method : using built-in function (join) 
+        public static string Smash2(string[] words)
+        {
+            return string.Join(" ",words);
+        }
+
+
+    }
+}
